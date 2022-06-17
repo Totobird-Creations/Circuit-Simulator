201f87f2555f94e1d17f7ae7963faf6489d34955
Totobird <>
2022-06-17 07:32:06 -0400

    Savepoint;

--- main/world/structure/strutcure.gd
+++ main/world/structure/strutcure.gd
@@ -5,13 +5,15 @@ class_name Structure
 
 enum State {
 	PENDING,
-	STATIONARY
+	WORLD,
+	LOCKED
 }
-var state : int = State.PENDING
+export(State) var state : int = State.PENDING
 
 
 
 func _physics_process(_delta : float) -> void:
 	match (state):
 		State.PENDING:
-			global_position = get_global_mouse_position()
+			var mouse := get_global_mouse_position() / 128.0
+			global_position = Vector2(floor(mouse.x), floor(mouse.y)) * 128.0 + Vector2(64.0, 64.0)
